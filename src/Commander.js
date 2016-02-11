b47aede0782ecaed8841edb49d62d7864d324b21
zlargon <zlargon@icloud.com>
2016-02-11 13:13:38 +0800

    implement Commander.option()

--- src/Commander.js
+++ src/Commander.js
@@ -1,7 +1,32 @@
 'use strict';
 const path = require('path');
+const Flag = require('./Flag');
 
 module.exports = class Commander {
+  constructor () {
+    this._option = {};
+  }
+
+  option (flags, description, action) {
+    const o = Flag.parse(flags);
+    o.desc = description;
+    o.action = typeof action === 'function' ? action.bind(this) : null;
+
+    // short
+    if (o.short !== null && this._option.hasOwnProperty(o.short)) {
+      throw new TypeError(`option '${o.short}' is repeated`);
+    }
+    this._option[o.short] = o;
+
+    // long
+    if (o.long !== null && this._option.hasOwnProperty(o.long)) {
+      throw new TypeError(`option '${o.long}' is repeated`);
+    }
+    this._option[o.long] = o;
+
+    return this;  // chaining pattern
+  }
+
   parse (_args) {
     if (!Array.isArray(_args)) {
       throw new TypeError('argument should be an array');
---
+++ test/commander.option.js
@@ -0,0 +1,93 @@
+'use strict';
+const Commander = require('../src/Commander');
+const expect = require('chai').expect;
+
+describe('commander.option', function () {
+
+  it('add foo', function () {
+    const program = new Commander();
+    program.option('-f, --foo', 'foo!!');
+
+    const foo = {
+      name: 'foo',
+      short: '-f',
+      long: '--foo',
+      boolType: true,
+      desc: 'foo!!',
+      action: null
+    };
+
+    expect(program._option['-f']).to.deep.equal(foo);
+    expect(program._option['--foo']).to.deep.equal(foo);
+  });
+
+  it('add fooBar with param', function () {
+    const program = new Commander();
+    program.option('-f, --foo-bar <var>', 'foo!! bar!!');
+
+    const fooBar = {
+      name: 'fooBar',
+      short: '-f',
+      long: '--foo-bar',
+      boolType: false,
+      desc: 'foo!! bar!!',
+      action: null
+    };
+
+    expect(program._option['-f']).to.deep.equal(fooBar);
+    expect(program._option['--foo-bar']).to.deep.equal(fooBar);
+  });
+
+  it('add foo, bar', function () {
+    const program = new Commander();
+    program.option('   -f   , --foo   ', 'foo!!');
+    expect(
+      program.option.bind(this, '-b, --BAR', 'bar!!')
+    ).to.throw(TypeError);
+  });
+
+  it('add foo, bar', function () {
+    const program = new Commander();
+    program.option('   -f   , --foo   ', 'foo!!');
+    program.option('-b, --bar <var>', 'bar!!');
+
+    const foo = {
+      name: 'foo',
+      short: '-f',
+      long: '--foo',
+      boolType: true,
+      desc: 'foo!!',
+      action: null
+    };
+
+    const bar = {
+      name: 'bar',
+      short: '-b',
+      long: '--bar',
+      boolType: false,
+      desc: 'bar!!',
+      action: null
+    };
+
+    expect(program._option['-f']).to.deep.equal(foo);
+    expect(program._option['--foo']).to.deep.equal(foo);
+    expect(program._option['-b']).to.deep.equal(bar);
+    expect(program._option['--bar']).to.deep.equal(bar);
+  });
+
+  it('add foo (repeated)', function () {
+    const program = new Commander();
+    expect(
+      program.option.bind(null, '--foo, --foo', 'foo!!')
+    ).to.throw(TypeError);
+  });
+
+  it('add bar (repeated)', function () {
+    const program = new Commander();
+    program.option('-b, --bar', 'bar!!');
+
+    expect(
+      program.option.bind(null, '-b, --bar', 'bar!!')
+    ).to.throw(TypeError);
+  });
+});
